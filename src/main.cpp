905de1573decaf55277aa3e7103f7d774f4d6dc8
Antonio <tcoy210@gmail.com>
2023-10-16 22:17:53 +0700

    Separated the main file and the extractor part

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,30 @@
-#include <stdio.h>
 #include <opencv2/opencv.hpp>
+
+#include "fastExtractor.h"
 
 using namespace cv;
 
-/* Gaussian filter */
-
-/*
- *
- */
-void fast_extractor(Mat* src_image, Mat* result, int threshold = 10, bool suppression = true,
-    FastFeatureDetector::DetectorType type = FastFeatureDetector::TYPE_9_16)
-{
-    //
-    Ptr<FastFeatureDetector> detector =
-        FastFeatureDetector::create(threshold, suppression, type);
-
-    //
-    std::vector<KeyPoint> keypoints;
-
-    //
-    detector->detect(*src_image, keypoints);
-
-    //
-    drawKeypoints(*src_image, keypoints, *result);
-}
 
 int main(int argc, char** argv)
 {
     Mat image, result;
     // Let's do a little slide show. Let's see how the algorithm processes 4 different images
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 9; i++)
     {
         // Saved the i-th image into an N-dimensional array
-        image = imread(format("data/%d.jpg", i), ImreadModes::IMREAD_GRAYSCALE);  // ImreadModes::IMREAD_GRAYSCALE
+        image = imread(format("data/%d.jpg", i));  // ImreadModes::IMREAD_GRAYSCALE
         if (!image.data) {
             printf("No image data \n");
             return -1;
         }
 
+        std::vector<KeyPoint> keypoints;
+
         // Applied the FAST algorithm to the image and saved the image
         // with the highlighted features in @result
-        fast_extractor(&image, &result, 13);
+        fastExtractor(&image, &keypoints, 13);
+
+        drawKeypoints(image, keypoints, result);
 
         namedWindow("Display Image", WINDOW_AUTOSIZE);
         imshow("Display Image", result);
