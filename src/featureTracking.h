946001fe6257681eb1b38002299005a9c6313671
IgorShal <i.shalygin@g.nsu.ru>
2023-10-26 21:36:52 +0700

    MVP without opencv magic

---
+++ src/featureTracking.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+
+
+
+void getPointsAroundFeature(cv::Point2f feature, int radius, double barier, std::vector<cv::Point2f>& pointsAround);
+
+double sumSquaredDifferences(std::vector<cv::Point2f>& batch1, std::vector<cv::Point2f>& batch2, cv::Mat& image1, cv::Mat& image2, double min);
+
+int trackFeature(cv::Point2f feature, cv::Mat& image1, cv::Mat& image2, cv::Point2f& res, double barier);
+
+void trackFeatures(std::vector<cv::Point2f>& features, cv::Mat& previousFrame, cv::Mat& currentFrame, std::vector<cv::Point2f>& newFeatures,
+	int barier, double maxAcceptableDifference);
