f99bb3ee7057181a7ae097e7fea6440e90e2d05d
Kirill Kozlov <kozlov.kirill.v.work@gmail.com>
2023-10-31 09:34:33 +0700

    Add Docs for camera transition module

--- src/cameraTransition.cpp
+++ src/cameraTransition.cpp
@@ -2,31 +2,29 @@
 #include <opencv2/calib3d.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/core/hal/hal.hpp>
+#include <random>
 
-#include "cameraCalibration.h"
 
 #include "cameraTransition.h"
 
 using namespace cv;
 
 
-void countMatrices(InputArray qPoints, InputArray gPoints, Mat& P)
+void estimateProjection(cv::InputArray points1, cv::InputArray points2, const cv::Mat& calibrationMatrix,
+                        cv::Mat& rotationMatrix, cv::Mat& translationVector, cv::Mat& projectionMatrix)
 {
-	Mat fundamentalMatrix = findFundamentalMat(qPoints, gPoints);
-
-	// How to find K camera calibration matrix?
-	Mat cameraCalib(3, 3, CV_32F);
-    calibration(cameraCalib, CalibrationOption::load);
 
     // Maybe it's have sense to undistort points and matrix K
 
-	Mat essentialMatrix = findEssentialMat(qPoints, gPoints, cameraCalib);
+	Mat essentialMatrix = findEssentialMat(points1, points2, calibrationMatrix);
+    std::cout  << "E:\n" << essentialMatrix << std::endl;
 
     // Find P matrix using wrapped SVD and
-    Mat cvR, cvt;
-    Mat p1(1, 2, CV_32F), p2(1, 2, CV_32F);
-    qPoints.getMat().row(0).copyTo(p1.row(0));
-    gPoints.getMat().row(0).copyTo(p2.row(0));
-    recoverPose(essentialMatrix, p1, p2, cvR, cvt);
-    hconcat(cvR, cvt, P);
+    int randomPointIndex = rand() % points1.rows();
+    Mat p1(1, 2, CV_64F), p2(1, 2, CV_64F);
+    points1.getMat().row(randomPointIndex).copyTo(p1.row(0));
+    points2.getMat().row(randomPointIndex).copyTo(p2.row(0));
+    std::cout << recoverPose(essentialMatrix, p1, p1, rotationMatrix, translationVector) << std::endl;
+    hconcat(rotationMatrix, translationVector, projectionMatrix);
+    std::cout << projectionMatrix << std:: endl;
 }
