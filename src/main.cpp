86d2dc9ab28b17d4f25034ee5f16498747d2c213
IgorShal <i.shalygin@g.nsu.ru>
2023-11-10 00:27:32 +0700

    first version

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <cstdio>
+#include <fstream>
 #include <iostream>
 
 #include "fastExtractor.h"
@@ -12,133 +13,78 @@
 
 #define ESC_KEY 27
 #define FEATURE_EXTRACTING_THRESHOLD 10
-#define FEATURE_TRACKING_BARRIER 10
+#define FEATURE_TRACKING_BARRIER 20
 #define FEATURE_TRACKING_MAX_ACCEPTABLE_DIFFERENCE 10000
 using namespace cv;
 
-// Checks if a matrix is a valid rotation matrix.
-bool isRotationMatrix(Mat& R)
-{
-    Mat Rt;
-    transpose(R, Rt);
-    Mat shouldBeIdentity = Rt * R;
-    Mat I = Mat::eye(3, 3, shouldBeIdentity.type());
 
-    return  norm(I, shouldBeIdentity) < 1e-6;
-
-}
-
-// Calculates rotation matrix to euler angles
-// The result is the same as MATLAB except the order
-// of the euler angles ( x and z are swapped ).
-Vec3f rotationMatrixToEulerAngles(Mat& R)
-{
-
-    assert(isRotationMatrix(R));
-
-    float sy = sqrt(R.at<double>(0, 0) * R.at<double>(0, 0) + R.at<double>(1, 0) * R.at<double>(1, 0));
-
-    bool singular = sy < 1e-6; // If
-
-    float x, y, z;
-    if (!singular)
-    {
-        x = atan2(R.at<double>(2, 1), R.at<double>(2, 2));
-        y = atan2(-R.at<double>(2, 0), sy);
-        z = atan2(R.at<double>(1, 0), R.at<double>(0, 0));
-    }
-    else
-    {
-        x = atan2(-R.at<double>(1, 2), R.at<double>(1, 1));
-        y = atan2(-R.at<double>(2, 0), sy);
-        z = 0;
-    }
-    return Vec3f(x, y, z);
-
-}
-
-#define TWO_FRAMES
+#define FRAMES_GAP 10
+#define REQUIRED_EXTRACTED_POINTS_COUNT 10
 
 int main(int argc, char** argv)
 {
-#ifdef TWO_FRAMES
-    reportingCycleForFramesPairs(10, 10, 10000);
-#else
-    Mat image, image2, result;
-    std::vector<KeyPoint> keypoints;
-
-
+	Mat currentFrame, previousFrame, result;
+	std::vector<KeyPoint> currentFrameExtractedKeyPoints;
+	std::vector<Point2f> currentFrameExtractedPoints;
+	std::vector<Point2f> previousFrameExtractedPoints;
+	std::vector<Point2f> currentFrameTrackedPoints;
+	std::ofstream reportStream;
+	char report[256];
+	reportStream.open(report);
 	VideoCapture cap("data/indoor_speed.mp4");
 	if (!cap.isOpened()) {
 		std::cerr << "Camera wasn't opened" << std::endl;
 		return -1;
 	}
 
-    Mat previousProjectionMatrix = (Mat_<double>(3, 4) << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0),
-            currentProjectionMatrix(3, 4, CV_64F);
 
-    Mat calibrationMatrix(3, 3, CV_64F);
-    calibration(calibrationMatrix, CalibrationOption::load);
+	Mat previousProjectionMatrix = (Mat_<double>(3, 4) << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0),
+		currentProjectionMatrix(3, 4, CV_64F);
 
-    while (true) {
-		cap.read(image);
-        cvtColor(image, image, COLOR_BGR2GRAY);
-        cvtColor(image, image, COLOR_GRAY2BGR);
+	Mat calibrationMatrix(3, 3, CV_64F);
+	calibration(calibrationMatrix, CalibrationOption::load);
 
-        // Applied the FAST algorithm to the image and saved the image
-        // with the highlighted features in @result
-        fastExtractor(image, keypoints, 10);
-        drawKeypoints(image, keypoints, result);
+	int countOfFrames = 0;
+	bool first = true;
+	while (true) {
+		cap.read(currentFrame);
+		countOfFrames++;
+		if (countOfFrames >= FRAMES_GAP) {
+			countOfFrames = 0;
+			cvtColor(currentFrame, currentFrame, COLOR_BGR2GRAY);
+			cvtColor(currentFrame, currentFrame, COLOR_GRAY2BGR);
+			fastExtractor(currentFrame, currentFrameExtractedKeyPoints, FEATURE_EXTRACTING_THRESHOLD);
+			if (currentFrameExtractedKeyPoints.size() < REQUIRED_EXTRACTED_POINTS_COUNT)
+				continue;
+			if (first) {
+				KeyPoint::convert(currentFrameExtractedKeyPoints, currentFrameExtractedPoints);
+				previousFrameExtractedPoints = currentFrameExtractedPoints;
+				previousFrame = currentFrame.clone();
+				first = false;
+				continue;
+			}
 
-        namedWindow("Display Image", WINDOW_AUTOSIZE);
-        imshow("Display Image", result);
-        waitKey(1000);
 
-        //Transform image into black and white
-		cap.read(image2);
-        cvtColor(image2, image2, COLOR_BGR2GRAY);
-        cvtColor(image2, image2, COLOR_GRAY2BGR);
+			trackFeatures(previousFrameExtractedPoints, currentFrameTrackedPoints, previousFrame,
+				currentFrame, FEATURE_TRACKING_BARRIER, FEATURE_TRACKING_MAX_ACCEPTABLE_DIFFERENCE);
+			Mat rotationMatrix = Mat::zeros(3, 3, CV_64F),
+				translationVector = Mat::zeros(3, 1, CV_64F);
+			if (estimateProjection(previousFrameExtractedPoints, currentFrameTrackedPoints, calibrationMatrix, rotationMatrix,
+				translationVector, currentProjectionMatrix)) {
+				Mat homogeneous3DPoints;
+				triangulate(previousFrameExtractedPoints, currentFrameTrackedPoints, previousProjectionMatrix,
+					currentProjectionMatrix, homogeneous3DPoints);
+				previousFrameExtractedPoints = currentFrameExtractedPoints;
+				previousProjectionMatrix = currentProjectionMatrix;
+				previousFrame = currentFrame;
+			}
+			reportStream << "Features extracted: " << previousFrameExtractedPoints.size() << std::endl;
+			reportStream << "Tracked points: " << currentFrameTrackedPoints.size() << std::endl;
+			reportStream << "Current projection matrix:\n" << currentProjectionMatrix << std::endl << std::endl;
 
-        std::vector<Point2f> features;
-        std::vector<Point2f> newFeatures;
-        KeyPoint::convert(keypoints, features);
-
-        trackFeatures(features, image, image2, newFeatures, 10, 10000);
-
-        //Getting keypoints vector to show from points vector(needed only for afcts, you can delete it)
-        std::vector<KeyPoint> keyPoints2;
-        KeyPoint::convert(newFeatures, keyPoints2);
-        drawKeypoints(image2, keyPoints2, result);
-        imshow("Display Image", result);
-
-        Mat q = Mat(features);
-        Mat g = Mat(newFeatures);
-        q = q.reshape(1);
-        g = g.reshape(1);
-
-        ////////////////////////////////////////
-        // Estimate matrices
-        ////////////////////////////////////////
-        currentProjectionMatrix = Mat(3, 4, CV_64F);
-        Mat rotationMatrix = Mat::zeros(3, 3, CV_64F),
-                translationVector = Mat::zeros(3, 1, CV_64F);
-        estimateProjection(q, g, calibrationMatrix, rotationMatrix,
-                           translationVector, currentProjectionMatrix);
-        ////////////////////////////////////////
-
-        Mat homogeneous3DPoints;
-        triangulate(q, g, previousProjectionMatrix,
-                    currentProjectionMatrix, homogeneous3DPoints);
-        previousProjectionMatrix = currentProjectionMatrix;
-
-        //////////////////////////////////////
-
-        char c = (char)waitKey(1000);
-        Vec3f res = rotationMatrixToEulerAngles(rotationMatrix);
-        std::cout << res << std::endl;
-        if (c == ESC_KEY)
-            break;
-    }
-#endif
-    return 0;
+		}
+		reportStream.flush();
+		reportStream.close();
+	}
+	return 0;
 }
