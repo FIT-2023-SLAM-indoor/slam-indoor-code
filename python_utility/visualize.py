843cf2ec33adabf3a71284304085cadccf2c77ee
Antonio <tcoy210@gmail.com>
2023-11-27 21:22:00 +0700

    Added camera controlling

--- python_utility/visualize.py
+++ python_utility/visualize.py
@@ -7,7 +7,11 @@ import math
 physicsClient = p.connect(p.GUI) # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,0)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+stadium = p.loadSDF("stadium.sdf")
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.configureDebugVisualizer(p.COV_ENABLE_MOUSE_PICKING, 1)
+p.configureDebugVisualizer(p.COV_ENABLE_KEYBOARD_SHORTCUTS, 1)
+p.resetDebugVisualizerCamera(cameraYaw=0.0, cameraPitch=-40.0,cameraDistance=7.5,cameraTargetPosition=[0.0,0.0,0.0])
 
 
 
@@ -15,12 +19,8 @@ file = open("../data/video_report/test.txt",'r')
 lst = file.readlines()
 file.close()
 
-p.configureDebugVisualizer(p.COV_ENABLE_MOUSE_PICKING, 1)
-p.configureDebugVisualizer(p.COV_ENABLE_KEYBOARD_SHORTCUTS, 1)
 
-p.loadSDF("stadium.sdf")
 
-#set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
 for i, val in enumerate(lst):  
     res = lst[i].replace('[', '').replace(']', '').replace(';', '').split(", ")
     if len(res) != 3:
@@ -31,8 +31,7 @@ for i, val in enumerate(lst):
 
     startOrientation = p.getQuaternionFromEuler([0,0,0])
     boxId = p.loadURDF("cube.urdf", startPos, startOrientation, globalScaling=0.12)
-    time.sleep(1./240.)
-
+    time.sleep(0.000001)
 
 for phi in range(0, 181, 10):
     for theta in range(0, 361, 18):
@@ -43,33 +42,71 @@ for phi in range(0, 181, 10):
         startOrientation = p.getQuaternionFromEuler([0,0,0])
         boxId = p.loadURDF("sphere2red.urdf", [x, y, z], startOrientation, globalScaling=0.12)
     
-    time.sleep(1./240.)
+    time.sleep(0.000001)
 
+
+def move_xyz(x, y, z):
+    p.resetDebugVisualizerCamera(cameraYaw=cam[8], cameraPitch=cam[9],cameraDistance=cam[10],cameraTargetPosition=[x, y, z])
+def move_yaw(yaw):
+    p.resetDebugVisualizerCamera(cameraYaw=yaw, cameraPitch=cam[9],cameraDistance=cam[10],cameraTargetPosition=cam[11])
+def move_pitch(pitch):
+    p.resetDebugVisualizerCamera(cameraYaw=cam[8], cameraPitch=pitch,cameraDistance=cam[10],cameraTargetPosition=cam[11])
+
+gui_flag = True
 while(1):
     keys = p.getKeyboardEvents()
     cam = p.getDebugVisualizerCamera()
-    #Keys to change camera
-    if keys.get(109):  #M
+
+    if keys.get(p.B3G_RIGHT_ARROW):  # >
         xyz = cam[11]
-        x= float(xyz[0]) + 0.125
+        x = float(xyz[0]) + 0.125
         y = xyz[1]
         z = xyz[2]
-    p.resetDebugVisualizerCamera(cameraYaw = cam[8], cameraPitch= cam[9],cameraDistance = cam[10],cameraTargetPosition=[x,y,z])
-    if keys.get(97):   #A
+        move_xyz(x, y, z)
+    if keys.get(p.B3G_LEFT_ARROW):  # <
         xyz = cam[11]
-        x= float(xyz[0]) - 0.125
+        x = float(xyz[0]) - 0.125
         y = xyz[1]
         z = xyz[2]
-    p.resetDebugVisualizerCamera(cameraYaw = cam[8], cameraPitch= cam[9],cameraDistance = cam[10],cameraTargetPosition=[x,y,z])
-    if keys.get(99):   #C
+        move_xyz(x, y, z)
+    if keys.get(p.B3G_UP_ARROW):  # ^
         xyz = cam[11]
         x = xyz[0] 
         y = float(xyz[1]) + 0.125
         z = xyz[2]
-    p.resetDebugVisualizerCamera(cameraYaw = cam[8], cameraPitch= cam[9],cameraDistance = cam[10],cameraTargetPosition=[x,y,z])
-    if keys.get(102):  #F
+        move_xyz(x, y, z)
+    if keys.get(p.B3G_DOWN_ARROW):  # v 
         xyz = cam[11]
         x = xyz[0] 
         y = float(xyz[1]) - 0.125
         z = xyz[2]
-        p.resetDebugVisualizerCamera(cameraYaw = cam[8], cameraPitch= cam[9],cameraDistance = cam[10],cameraTargetPosition=[x,y,z])
+        move_xyz(x, y, z)
+    
+    if keys.get(ord('z')):  # Roll left
+        yaw = cam[8] + 1.25
+        move_yaw(yaw)
+    if keys.get(ord('x')):  # Roll right
+        yaw = cam[8] - 1.25
+        move_yaw(yaw)
+
+    if keys.get(ord('f')):  # Roll up
+        pitch = cam[9] - 1.25
+        move_pitch(pitch)
+    if keys.get(ord('c')):  # Roll down
+        pitch = cam[9] + 1.25
+        move_pitch(pitch)
+    
+    if keys.get(ord('q')):  # Z up
+        xyz = cam[11]
+        x = xyz[0]
+        y = xyz[1]
+        z = float(xyz[2]) + 0.125
+        move_xyz(x, y, z)
+    if keys.get(ord('a')):  # Z down
+        xyz = cam[11]
+        x = xyz[0]
+        y = xyz[1]
+        z = float(xyz[2]) - 0.125
+        move_xyz(x, y, z)
+
+    time.sleep(0.000001)
