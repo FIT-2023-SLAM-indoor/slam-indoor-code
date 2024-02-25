281aeaf9f8c060e73a8098380ce1e136c2655bf5
AntonTsoy <tcoy210@gmail.com>
2024-02-25 12:40:38 +0700

    Trasnfered XML functions

--- src/IOmisc.cpp
+++ src/IOmisc.cpp
@@ -1,7 +1,17 @@
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc.hpp>
+#include <ctime>
+#include <iostream>
+
+#include "main_config.h"
 #include "IOmisc.h"
 
-void sortGlobs(std::vector<String>& paths) {
-    std::sort(paths.begin(), paths.end(), [](const String& a, const String& b){
+
+void sortGlobs(std::vector<String> &paths) {
+    std::sort(paths.begin(), paths.end(), [](const String &a, const String &b) {
         int aLen = a.length(), bLen = b.length();
         if (aLen != bLen)
             return aLen < bLen;
@@ -13,6 +23,41 @@ void sortGlobs(std::vector<String>& paths) {
             p *= 10;
             startDigitIndex++;
         }
-        return a < b;
+        return a < b; 
     });
+}
+
+void saveMatrixToXML(
+    const char *pathToXML, const Mat &matrix, const String &matrixKey, FileStorage::Mode mode)
+{
+
+    if (mode != FileStorage::WRITE && mode != FileStorage::APPEND)
+        std::cerr << "Only WRITE or APPEND mode can be used for save function" << std::endl;
+    FileStorage fs;
+    if (!fs.open(pathToXML, mode))
+    {
+        std::cerr << format("Cannot open %s", pathToXML) << std::endl;
+        exit(-1);
+    }
+    fs << matrixKey << matrix;
+}
+
+void saveCalibParametersToXML(
+    const char *pathToXML, const Mat &cameraMatrixK, 
+    const Mat &distortionCoeffs, const Mat &R, const Mat &T)
+{
+    saveMatrixToXML(pathToXML, cameraMatrixK);
+    saveMatrixToXML(pathToXML, distortionCoeffs, "DC", FileStorage::APPEND);
+    saveMatrixToXML(pathToXML, R, "R", FileStorage::APPEND);
+    saveMatrixToXML(pathToXML, T, "T", FileStorage::APPEND);
+}
+
+void loadMatrixFromXML(const char *pathToXML, Mat &matrix, const String &matrixKey) {
+    FileStorage fs;
+    if (!fs.open(pathToXML, FileStorage::READ))
+    {
+        std::cerr << format("Cannot open %s", pathToXML) << std::endl;
+        exit(-1);
+    }
+    fs[matrixKey] >> matrix;
 }
