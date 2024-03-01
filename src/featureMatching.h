355f0ab87cf8f705c6335fc87f771e27d44d5f90
IgorShal <i.shalygin@g.nsu.ru>
2024-03-01 19:12:38 +0700

    Did you get what you want?

--- src/featureMatching.h
+++ src/featureMatching.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <opencv2/opencv.hpp>
 using namespace cv;
-/**
-* Feature matching function. Use defines FM_SIFT_BF FM_ORB and FM_SIFT_FLANN to select your FM algo.
-*/
-void featureMatching(Mat& previousFrame, Mat& currentFrame, std::vector<KeyPoint>& previousFeatures, std::vector<KeyPoint>& currentFeatures,
-	std::vector<Point2f>& trackedFeatures, std::vector<Point2f>& newPreviousFeatures);
+
+void getMatchedPoints(Mat& previousFrame, Mat& currentFrame, std::vector<KeyPoint>& previousFeatures, std::vector<KeyPoint>& currentFeatures,
+	std::vector<Point2f>& matchedFeatures, std::vector<Point2f>& newPreviousFeatures, int matcherType, int radius, bool showMatchedPoints);
+
+void showMatchedPointsInTwoFrames(std::vector<KeyPoint>& previousFeatures, std::vector<KeyPoint>& currentFeatures,
+	Mat& previousFrame, Mat& currentFrame, std::vector<std::vector<DMatch>>& matches);
+
+void extractDescriptorsAndMatch(Mat& previousFrame, Mat& currentFrame, std::vector<KeyPoint>& previousFeatures, std::vector<KeyPoint>& currentFeatures,
+	Mat& prevImgDesc, Mat& curImgDesc, int matcherType, std::vector<std::vector<DMatch>>& matches, int radius);
+
+void getGoodMatches(std::vector<KeyPoint>& previousFeatures, std::vector<KeyPoint>& currentFeatures,
+	std::vector<Point2f>& matchedFeatures, std::vector<Point2f>& newPreviousFeatures, std::vector<std::vector<DMatch>>& matches);
