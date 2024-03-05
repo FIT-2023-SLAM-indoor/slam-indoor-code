1c9836cb2ea19ad563950a42efc1b24791f90231
AntonTsoy <tcoy210@gmail.com>
2024-03-05 14:33:05 +0700

    Commit before merging with new matcheing

--- src/mainCycle.cpp
+++ src/mainCycle.cpp
@@ -7,39 +7,41 @@
 #include "IOmisc.h"
 #include "mainCycle.h"
 
+#define OPTIMIZE_DEQUE_SIZE 8
 
 using namespace cv;
 
 /*
-Список того, на что я решил забить:
+Список того, на что я пока решил забить:
 1) Сохранение цветов фич - надо немного переписать Extractor (ну или добавить это в функции поиска frame-ов)
 2) Выбор применения Undistortion-а к кадрам - сейчас он просто применяется
 3) Обработка крайних случаев: этот код нужно хорошо отревьюить, я толком не думал про небезопасные места
 4) Надо ли постоянно вызывать .clear() при создании новых объектов?
-5) 
+5) Указывать числам тип в виде uint32_t вместо int
 */
 
 bool findFirstGoodVideoFrameAndFeatures(
-    VideoCapture &frameSequence, 
+    VideoCapture &frameSequence,
     Mat &calibrationMatrix, Mat &distCoeffs,
     int featureExtractingThreshold,
     int requiredExtractedPointsCount,
     Mat &goodFrame,
-    std::vector<KeyPoint> &goodFrameFeatures) 
+    std::vector<KeyPoint> &goodFrameFeatures)
 {
     Mat candidateFrame;
-    while (frameSequence.read(candidateFrame)) {
+    while (frameSequence.read(candidateFrame))
+    {
         undistort(candidateFrame, goodFrame, calibrationMatrix, distCoeffs);
         fastExtractor(goodFrame, goodFrameFeatures, featureExtractingThreshold);
-        
-        if (goodFrameFeatures.size() >= requiredExtractedPointsCount) {
+
+        if (goodFrameFeatures.size() >= requiredExtractedPointsCount)
+        {
             return true;
         }
     }
 
     return false;
 }
-
 
 void createVideoFrameBatch(
     VideoCapture &frameSequence,
@@ -48,12 +50,12 @@ void createVideoFrameBatch(
 {
     int currentFrameBatchSize = 0;
     Mat nextFrame;
-    while (currentFrameBatchSize < frameBatchSize && frameSequence.read(nextFrame)) {
-        frameBatch.push_back(nextFrame);  // мб тут нужен .clone(), но вроде не нужен - я проверял
+    while (currentFrameBatchSize < frameBatchSize && frameSequence.read(nextFrame))
+    {
+        frameBatch.push_back(nextFrame); // мб тут нужен .clone(), но вроде не нужен - я проверял
         currentFrameBatchSize++;
     }
 }
-
 
 bool findGoodVideoFrameFromBatch(
     VideoCapture &frameSequence,
@@ -61,7 +63,7 @@ bool findGoodVideoFrameFromBatch(
     int frameBatchSize,
     int featureExtractingThreshold,
     int requiredMatchedPointsCount,
-    Mat &previousFrame, 
+    Mat &previousFrame,
     std::vector<KeyPoint> &previousFeatures,
     std::vector<Point2f> &previousMatchedFeatures,
     Mat &newGoodFrame,
@@ -70,17 +72,18 @@ bool findGoodVideoFrameFromBatch(
 {
     std::vector<Mat> frameBatch;
     createVideoFrameBatch(frameSequence, frameBatchSize, frameBatch);
-    
+
     Mat candidateFrame;
-    for (int frameIndex = frameBatch.size() - 1; frameIndex >= 0; frameIndex--) {
+    for (int frameIndex = frameBatch.size() - 1; frameIndex >= 0; frameIndex--)
+    {
         candidateFrame = frameBatch.at(frameIndex);
         undistort(candidateFrame, newGoodFrame, calibrationMatrix, distCoeffs);
         fastExtractor(newGoodFrame, newFeatures, featureExtractingThreshold);
         featureMatching(
             previousFrame, newGoodFrame, previousFeatures, newFeatures,
-            newMatchedFeatures, previousMatchedFeatures
-        );
-        if (newMatchedFeatures.size() >= requiredMatchedPointsCount) {
+            newMatchedFeatures, previousMatchedFeatures);
+        if (newMatchedFeatures.size() >= requiredMatchedPointsCount)
+        {
             return true;
         }
     }
@@ -88,6 +91,40 @@ bool findGoodVideoFrameFromBatch(
     return false;
 }
 
+bool processingFirstPairFrames(
+    VideoCapture &frameSequence,
+    Mat &calibrationMatrix, Mat &distCoeffs,
+    int featureExtractingThreshold,
+    int requiredExtractedPointsCount,
+    int frameBatchSize,
+    std::deque<TemporalImageData> &temporalImageDataDeque) 
+{
+
+    Mat firstFrame;
+    if (!findFirstGoodVideoFrameAndFeatures(
+            frameSequence,
+            calibrationMatrix, distCoeffs,
+            featureExtractingThreshold,
+            requiredExtractedPointsCount,
+            firstFrame,
+            temporalImageDataDeque.at(0).allExtractedFeatures))
+    {
+        return false;
+    }
+    temporalImageDataDeque.at(0).rotation = Mat::eye(3, 3, CV_64FC1);
+    temporalImageDataDeque.at(0).motion = Mat::zeros(3, 1, CV_64FC1);
+
+    Mat secondFrame;
+    /*
+    findGoodVideoFrameFromBatch(
+        frameSequence, calibrationMatrix, distCoeffs, frameBatchSize, 
+        featureExtractingThreshold, 
+        requiredExtractedPointsCount, 
+        firstFrame, 
+        temporalImageDataDeque.at(0).allExtractedFeatures,
+        )
+    */
+}
 
 void videoCycle(
     VideoCapture &frameSequence,
@@ -96,17 +133,10 @@ void videoCycle(
 {
     Mat calibrationMatrix(3, 3, CV_64F);
     Mat distCoeffs(1, 5, CV_64F);
-	calibration(calibrationMatrix, CalibrationOption::load);
+    calibration(calibrationMatrix, CalibrationOption::load);
     loadMatrixFromXML(CALIBRATION_PATH, distCoeffs, "DC");
 
-    Mat firstFrame;
-    std::vector<TemporalImageData> temporalImageDataQueue(8);
-    findFirstGoodVideoFrameAndFeatures(
-        frameSequence,
-        calibrationMatrix, distCoeffs,
-        featureExtractingThreshold,
-        requiredExtractedPointsCount,
-        firstFrame,
-        temporalImageDataQueue.at(0).allExtractedFeatures
-    );
+    std::deque<TemporalImageData> temporalImageDataDeque(OPTIMIZE_DEQUE_SIZE);
+    
+    
 }
