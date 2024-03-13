d2e9c0916fbfeca0cc673a932efa848522ac67f0
AntonTsoy <tcoy210@gmail.com>
2024-03-13 22:32:30 +0700

    Finish photo cycle adoptation task

--- src/main.cpp
+++ src/main.cpp
@@ -31,28 +31,19 @@ int main(int argc, char** argv)
 		return 0;
 	}
 	std::string path = configService.getValue<std::string>(ConfigFieldEnum::OUTPUT_DATA_DIR_);
-	if (configService.getValue<bool>(ConfigFieldEnum::USE_PHOTOS_CYCLE)) {
-		std::vector<String> photos;
-		glob(configService.getValue<std::string>(ConfigFieldEnum::PHOTOS_PATH_PATTERN_), photos, false);
-		sortGlobs(photos);
-//		photosProcessingCycle(photos,
-//							  FT_BARRIER,
-//							  FT_MAX_ACCEPTABLE_DIFFERENCE,
-//							  FRAMES_BATCH_SIZE,
-//							  REQUIRED_EXTRACTED_POINTS_COUNT,
-//							  FEATURE_EXTRACTING_THRESHOLD,
-//							  path);
-	}
-	else {
-		mainCycle(
-			configService.getValue<int>(ConfigFieldEnum::FRAMES_BATCH_SIZE_),
-			configService.getValue<int>(ConfigFieldEnum::FEATURE_EXTRACTING_THRESHOLD_),
-			configService.getValue<int>(ConfigFieldEnum::REQUIRED_EXTRACTED_POINTS_COUNT_),
-			configService.getValue<int>(ConfigFieldEnum::REQUIRED_EXTRACTED_POINTS_COUNT_),
-			getMatcherTypeIndex(),
-			configService.getValue<float>(ConfigFieldEnum::FM_SEARCH_RADIUS_)
-	   );
-	}
+
+	/////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////
+	mainCycle(
+		configService.getValue<int>(ConfigFieldEnum::FRAMES_BATCH_SIZE_),
+		configService.getValue<int>(ConfigFieldEnum::FEATURE_EXTRACTING_THRESHOLD_),
+		configService.getValue<int>(ConfigFieldEnum::REQUIRED_EXTRACTED_POINTS_COUNT_),
+		configService.getValue<int>(ConfigFieldEnum::REQUIRED_EXTRACTED_POINTS_COUNT_),
+		getMatcherTypeIndex(),
+		configService.getValue<float>(ConfigFieldEnum::FM_SEARCH_RADIUS_));
+	/////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////
+
 	closeLogsStreams();
     return 0;
 }
