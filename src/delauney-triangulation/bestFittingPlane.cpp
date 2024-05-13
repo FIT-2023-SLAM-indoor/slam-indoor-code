071d11d166078cab69b8485e21f7ad6485b85333
IgorShal <i.shalygin@g.nsu.ru>
2024-05-13 23:42:49 +0700

    fixed bug + fixed stupid mesh construction

--- src/delauney-triangulation/bestFittingPlane.cpp
+++ src/delauney-triangulation/bestFittingPlane.cpp
@@ -9,9 +9,9 @@
 using namespace cv;
 using namespace std;
 void getBestFittingPlaneByPoints(std::vector<Point3f>& points, Point3f& centroid, Vec3d& normal){
-    Mat A = Mat(points.size(),3,CV_32F,points.data());
+    Mat A = Mat(points.size(),3,CV_32F,points.data()).clone();
     transpose(A,A);
-    cout<< A <<endl;
+    //cout<< A <<endl;
     Mat leftSingularVectors, leftSingularValues, transposedMatrixOfRightSingularVectors;
     
 
@@ -42,26 +42,32 @@ void getBestFittingPlaneByPoints(std::vector<Point3f>& points, Point3f& centroid
 
 int test() {
     vector<pair<Point2f,Point3f>> pairs;
+    vector<pair<int,Point3f>> pairs_index;
+
     vector<Point3f> points;
+    
     
     /*
     points.push_back(Point3f(0.0, 0.0, 0.0));
     points.push_back(Point3f(70.0, 0.0, 0.0));
     points.push_back(Point3f(70.0, 50.0, 0.0));
     points.push_back(Point3f(70.0, 50.0, 70.0));
-
+    
     points.push_back(Point3f(0.0, 49.0, 70.0));
+    
     points.push_back(Point3f(0.0, 0.0, 70.0));
     points.push_back(Point3f(0.0, 50.0, 0.0));
-    points.push_back(Point3f(70.0, 0.0, 70.0));*/
+    points.push_back(Point3f(70.0, 0.0, 70.0));
+    */
     srand(time(0));
-    for (int i = 0;i< 1000;i++){
+   
+    srand(time(0));
+    for (int i = 0;i< 10000;i++){
         int x = rand()%3000;
         int y = 100+ rand()%300 - rand()%300;
         int z = rand()%3000;
         points.push_back(Point3f(x,y,z));
     }
-
     
 
     
@@ -79,9 +85,11 @@ int test() {
     cout<< "centroid:" << centroid << endl;
     viz::WPlane bestFittingPlane(centroid,normal,Vec3d(1,1,1),Size2d(Point2d(150,150)));
     std::vector<Point3f> projectedPoints;
+
     for (int i = 0;i < points.size();i++){
         Point3f projectedPoint;
-        
+        pairs_index.push_back(pair(i,points.at(i)));
+
         projectPointOnPlane(points.at(i),normal,centroid,projectedPoint);
         //projectedPoints.push_back(projectedPoint);
         
@@ -108,7 +116,9 @@ int test() {
         Point2f cur  = Point2f(projectedPoints.at(i).x,projectedPoints.at(i).z);
         pts.push_back(cur);
         pairs.push_back(pair(cur,points.at(i)));
+
     }
+    /*
     vector<Vec3b> proj2Colors;
     for (int i = 0;i< projectedPoints.size();i++){
         proj2Colors.push_back(viz::Color::celestial_blue());
@@ -116,11 +126,11 @@ int test() {
     viz::WCloud projectedPointsWidget = getPointCloudFromPoints(projectedPoints,proj2Colors);
     //projectedPointsWidget.setRenderingProperty( cv::viz::POINT_SIZE, 10);
     window.showWidget("point_cloud22", projectedPointsWidget);
-
+    */
 
 
     cloudWidget.setRenderingProperty( cv::viz::POINT_SIZE, 10);
-    window.showWidget("point_cloud", cloudWidget);
+    //window.showWidget("point_cloud", cloudWidget);
     window.showWidget("coordinate", viz::WCoordinateSystem(100));
     //window.showWidget("bestPlane",bestFittingPlane);
 
@@ -136,47 +146,62 @@ int test() {
     vector<Vec3b> projColors;
 
     vector<Triangle> triang;
+    std::cout << "Triangulation:" << endl;
     triangulation(pts,triang);
-    cout << "Triangulation:" << endl;
+    
+    
+    
+
+    vector<int> poly;
     for (int i = 0;i< triang.size();i++){
         bool flag = true;
+        poly.push_back(3);
 
-        cv::Mat polygon3 = (cv::Mat_<int>(1,4) << 3, 0, 1, 2);
-        vector<int> faces{3, 0, 1, 2};
-        vector<Point3f> cloud3;
-        cout << triang.at(i).points << endl;
+        //std::cout << triang.at(i).points << endl;
         for (int j = 0;j < 3;j++){
             Point3f pt;
             for (int k = 0;k< pairs.size();k++){
                 if (pairs.at(k).first.x == triang.at(i).points.at(j).x && 
                 pairs.at(k).first.y == triang.at(i).points.at(j).y){
                     pt = pairs.at(k).second;
+                    poly.push_back(k);
                     break;
                 }
+                
 
             }
-            cloud3.push_back(pt);
-            if (j > 0 && distance(cloud3.at(j),cloud3.at(j-1)) > maxDistance){
-                flag = false;
-            }
+            
+            //if (j > 0 && distance(cloud3.at(j),cloud3.at(j-1)) > maxDistance){
+            //    flag = false;
+            //}
         }
         
         if (!flag){
             continue;
         }
-        cv::viz::WMesh trWidget(cloud3, faces);
-
-        trWidget.setColor(viz::Color::indigo());
-        trWidget.setRenderingProperty(viz::OPACITY, 0.005 * (i+1));
-        trWidget.setRenderingProperty(viz::SHADING, viz::SHADING_FLAT);
-        trWidget.setRenderingProperty(viz::REPRESENTATION, viz::REPRESENTATION_SURFACE);
-        String str = to_string(i);
-        window.showWidget(str, trWidget);
+        
     }
+    cv::Mat polygon3 = cv::Mat(poly).t();
+    //cout << polygon3 << endl;
+    vector<Vec3b> vectorColors;
+    for (int i = 0;i< points.size();i++){
+        int x = rand()%255;
+        int y = rand()%255;
+        int z = rand()%255;
+        vectorColors.push_back(Vec3b(x,y,z));
+    }
+    cv::viz::WMesh trWidget(points, polygon3,vectorColors);
+
+    //trWidget.setColor(viz::Color::indigo());
+    trWidget.setRenderingProperty(viz::OPACITY, 0.1);
+    trWidget.setRenderingProperty(viz::SHADING, viz::SHADING_FLAT);
+    trWidget.setRenderingProperty(viz::REPRESENTATION, viz::REPRESENTATION_SURFACE);
+
+    window.showWidget("mesh", trWidget);
 
 
     cout << "Triangulation ended" << endl;
-
+    /*
     vector<Point3f> trPts;
     for (int i = 0;i< pts.size();i++){
         trPts.push_back(Point3f(pts.at(i).x,0,pts.at(i).y));
@@ -185,6 +210,7 @@ int test() {
     viz::WCloud trPtsWidget = getPointCloudFromPoints(trPts,projColors);
     trPtsWidget.setRenderingProperty( cv::viz::POINT_SIZE, 5);
     //window.showWidget("pts",trPtsWidget);
+    */
     startWindowSpin(window);
 
 
